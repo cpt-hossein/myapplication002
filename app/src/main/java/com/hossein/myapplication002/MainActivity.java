05a2fd93cb91798bf62d1784ed0394989cf38843
cpt-hossein <h04524223946>
2021-11-13 17:14:19 +0330

    add id and views

--- app/src/main/java/com/hossein/myapplication002/MainActivity.java
+++ app/src/main/java/com/hossein/myapplication002/MainActivity.java
@@ -3,12 +3,22 @@ package com.hossein.myapplication002;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btn1;
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         btn1 = findViewById(R.id.btn1);
+         tv1  = findViewById(R.id.tv1);
+
+
+
     }
 }
